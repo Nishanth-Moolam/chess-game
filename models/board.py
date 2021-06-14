d1ef2afbc764e939b426c29d163c5db71bd041c7
Nishanth Moolam <nishanthmoolam@Nishanths-MacBook-Pro.local>
2021-06-13 20:28:37 -0400

    added to Piece and Board models

--- models/board.py
+++ models/board.py
@@ -2,4 +2,18 @@
 
 class Board:
     def __init__(self):
+        self.pieces = []
+        self.graveyard = []
+
+    def add_piece(self, piece):
         pass
+
+    def move_to_graveyard(self, piece):
+        pass
+
+    def update_board(self):
+        pass
+
+    
+
+    
